15c6deafa22ff3d7d423cebbf8ea3ed07c78489b
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-08-21 02:06:52 +0200

    Parse user input

---
+++ src/Lambda.elm
@@ -0,0 +1,311 @@
+module Lambda exposing (..)
+
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (style)
+import Maybe
+import String
+
+
+getAt : Int -> List a -> Maybe a
+getAt n xs =
+    case xs of
+        [] ->
+            Nothing
+
+        y :: ys ->
+            if n == 0 then
+                Just y
+
+            else
+                getAt (n - 1) ys
+
+
+orElse : (() -> Maybe a) -> Maybe a -> Maybe a
+orElse deferred evaluated =
+    case evaluated of
+        Just _ ->
+            evaluated
+
+        Nothing ->
+            deferred ()
+
+
+type alias VarName =
+    String
+
+
+type Lambda
+    = Var Int
+    | App Lambda Lambda
+    | Lam VarName Lambda
+
+
+lambdaToString : Lambda -> Maybe String
+lambdaToString lambda =
+    let
+        toStringWithStack : Lambda -> List VarName -> Maybe String
+        toStringWithStack l stack =
+            case l of
+                Var t ->
+                    getAt t stack |> Maybe.map (\name -> name ++ String.fromInt t)
+
+                App f x ->
+                    case ( toStringWithStack f stack, toStringWithStack x stack ) of
+                        ( Just fString, Just xString ) ->
+                            Just (addParensL f fString ++ " " ++ addParensR x xString)
+
+                        _ ->
+                            Nothing
+
+                Lam name f ->
+                    Maybe.map (\x -> "λ" ++ name ++ ". " ++ x) (toStringWithStack f (name :: stack))
+    in
+    toStringWithStack lambda []
+
+
+toDebugString : Lambda -> String
+toDebugString lambda =
+    let
+        toDebugStringWithStack : Lambda -> List VarName -> String
+        toDebugStringWithStack l stack =
+            case l of
+                Var t ->
+                    getAt t stack |> Maybe.map (\name -> name ++ String.fromInt t) |> Maybe.withDefault (String.fromInt t)
+
+                App f x ->
+                    let
+                        left =
+                            toDebugStringWithStack f stack
+
+                        right =
+                            toDebugStringWithStack x stack
+                    in
+                    addParensL f left ++ " " ++ addParensR x right
+
+                Lam name f ->
+                    "λ" ++ name ++ ". " ++ toDebugStringWithStack f (name :: stack)
+    in
+    toDebugStringWithStack lambda []
+
+
+addParensL : Lambda -> String -> String
+addParensL lambda rendered =
+    case lambda of
+        Lam _ _ ->
+            "(" ++ rendered ++ ")"
+
+        _ ->
+            rendered
+
+
+addParensR : Lambda -> String -> String
+addParensR lambda rendered =
+    case lambda of
+        Var _ ->
+            rendered
+
+        _ ->
+            "(" ++ rendered ++ ")"
+
+
+type Direction
+    = Left
+    | Down
+    | Right
+
+
+type alias Location =
+    List Direction
+
+
+isBetaReducible : Lambda -> Bool
+isBetaReducible lambda =
+    case lambda of
+        App (Lam _ _) _ ->
+            True
+
+        _ ->
+            False
+
+
+tryBetaReduce : Lambda -> Maybe Lambda
+tryBetaReduce lambda =
+    let
+        adjustFreeVars : Int -> Lambda -> Int -> Lambda
+        adjustFreeVars increase lam depth =
+            case lam of
+                Var v ->
+                    if v < depth then
+                        Var v
+
+                    else
+                        Var (v + increase)
+
+                App left right ->
+                    App
+                        (adjustFreeVars increase left depth)
+                        (adjustFreeVars increase right depth)
+
+                Lam name body ->
+                    Lam name
+                        (adjustFreeVars increase body (depth + 1))
+
+        substituteTopVar : Int -> Lambda -> Lambda -> Lambda
+        substituteTopVar depth arg lam =
+            case lam of
+                Var v ->
+                    case compare v depth of
+                        LT ->
+                            Var v
+
+                        EQ ->
+                            adjustFreeVars depth arg 0
+
+                        GT ->
+                            Var (v - 1)
+
+                App left right ->
+                    App
+                        (substituteTopVar depth arg left)
+                        (substituteTopVar depth arg right)
+
+                Lam name body ->
+                    Lam name
+                        (substituteTopVar (depth + 1) arg body)
+    in
+    case lambda of
+        App (Lam _ body) arg ->
+            Just (substituteTopVar 0 arg body)
+
+        _ ->
+            Nothing
+
+
+findBetas : Lambda -> List Location
+findBetas lambda =
+    case lambda of
+        App left right ->
+            (if isBetaReducible lambda then
+                [ [] ]
+
+             else
+                []
+            )
+                ++ (findBetas left |> List.map (\p -> Left :: p))
+                ++ (findBetas right |> List.map (\p -> Right :: p))
+
+        Lam _ body ->
+            findBetas body |> List.map (\p -> Down :: p)
+
+        Var _ ->
+            []
+
+
+betaReduceAt : Location -> Lambda -> Maybe Lambda
+betaReduceAt location lambda =
+    case ( location, lambda ) of
+        ( [], _ ) ->
+            tryBetaReduce lambda
+
+        ( Down :: path, Lam _ body ) ->
+            betaReduceAt path body
+
+        ( Left :: path, App left _ ) ->
+            betaReduceAt path left
+
+        ( Right :: path, App _ right ) ->
+            betaReduceAt path right
+
+        _ ->
+            Nothing
+
+
+betaReduceAnywhere : Lambda -> Maybe Lambda
+betaReduceAnywhere lambda =
+    case lambda of
+        Var _ ->
+            Nothing
+
+        Lam name body ->
+            betaReduceAnywhere body |> Maybe.map (\b -> Lam name b)
+
+        App left right ->
+            tryBetaReduce lambda
+                |> orElse (\_ -> betaReduceAnywhere left |> Maybe.map (\l -> App l right))
+                |> orElse (\_ -> betaReduceAnywhere right |> Maybe.map (\r -> App left r))
+
+
+type FullReductionResult
+    = Terminated (List Lambda)
+    | Unterminated (List Lambda)
+
+
+mapFullReductionResult : (List Lambda -> List Lambda) -> FullReductionResult -> FullReductionResult
+mapFullReductionResult f result =
+    case result of
+        Terminated xs ->
+            Terminated (f xs)
+
+        Unterminated xs ->
+            Unterminated (f xs)
+
+
+fullyBetaReduce : Int -> Lambda -> FullReductionResult
+fullyBetaReduce fuel lambda =
+    mapFullReductionResult (\ls -> lambda :: ls) <|
+        case ( fuel, betaReduceAnywhere lambda ) of
+            ( 0, Just _ ) ->
+                Unterminated []
+
+            ( _, Just reduced ) ->
+                fullyBetaReduce (fuel - 1) reduced
+
+            ( _, Nothing ) ->
+                Terminated []
+
+
+naszaLambda =
+    Lam "x" (Lam "y" (App (Var 1) (Var 0)))
+
+
+kCombinator =
+    Lam "x" (Lam "y" (Var 1))
+
+
+sCombinator =
+    Lam "x" (Lam "y" (Lam "z" (App (App (Var 2) (Var 0)) (App (Var 1) (Var 0)))))
+
+
+skk =
+    App (App sCombinator kCombinator) kCombinator
+
+
+ksk =
+    App kCombinator (App sCombinator kCombinator)
+
+
+sus =
+    Lam "x" (App (App (Var 0) (Var 0)) (Var 0))
+
+
+sus2 =
+    App sus sus
+
+
+displayReductionSteps : List Lambda -> Html msg
+displayReductionSteps lambdas =
+    lambdas
+        |> List.map (lambdaToString >> Maybe.withDefault "error" >> text >> (\t -> div [] [ t ]))
+        |> div [ style "display" "flex", style "flex-direction" "column" ]
+
+
+
+oldMain = div []
+        [ findBetas ksk |> Debug.toString |> text
+        , case fullyBetaReduce 4 skk of
+            Terminated list ->
+                div [] [ text "yay", displayReductionSteps list ]
+
+            Unterminated list ->
+                div [] [ text "nay", displayReductionSteps list ]
+        ]
---
+++ src/LambdaParser.elm
@@ -0,0 +1,144 @@
+module LambdaParser exposing (..)
+
+import Lambda exposing (..)
+import List exposing (length, member, tail)
+import Parser exposing (..)
+import String exposing (indexes)
+import List exposing (foldr, foldl)
+
+
+type ParsedLambda
+    = PVar String
+    | PApps (List ParsedLambda)
+    | PLam String ParsedLambda
+
+
+parseLambda : String -> Maybe Lambda
+parseLambda str =
+    let
+        parsingResult =
+            run lambdaParser str
+    in
+    case parsingResult of
+        Err _ ->
+            Nothing
+
+        Ok parsed ->
+            buildLambda parsed
+
+
+termParser : Parser ParsedLambda
+termParser =
+    oneOf
+        [ varParser
+        , lamParser
+        , parensParser
+        ]
+
+
+varNameParser : Parser String
+varNameParser =
+    getChompedString <|
+        succeed ()
+            |. chompIf Char.isLower
+            |. chompWhile (\c -> Char.isAlphaNum c || c == '_')
+
+
+varParser : Parser ParsedLambda
+varParser =
+    succeed PVar |= varNameParser
+
+
+lamParser : Parser ParsedLambda
+lamParser =
+    succeed PLam
+        |. symbol "\\"
+        |. spaces
+        |= varNameParser
+        |. spaces
+        |. symbol "."
+        |. spaces
+        |= lazy (\_ -> lambdaParser)
+
+
+parensParser : Parser ParsedLambda
+parensParser =
+    succeed identity
+        |. symbol "("
+        |. spaces
+        |= lazy (\_ -> lambdaParser)
+        |. spaces
+        |. symbol ")"
+
+
+lambdaParser : Parser ParsedLambda
+lambdaParser =
+    succeed PApps |= loop [] lambdaParserHelper
+
+
+lambdaParserHelper : List ParsedLambda -> Parser (Step (List ParsedLambda) (List ParsedLambda))
+lambdaParserHelper revLambdas =
+    oneOf
+        [ succeed (\lambda -> Loop (lambda :: revLambdas)) |= termParser |. spaces
+        , succeed () |> map (\_ -> Done (List.reverse revLambdas))
+        ]
+
+
+buildLambda : ParsedLambda -> Maybe Lambda
+buildLambda parsed =
+    buildLambda_ parsed []
+
+
+buildLambda_ : ParsedLambda -> List VarName -> Maybe Lambda
+buildLambda_ parsed varStack =
+    let _ = Debug.log "aaa" parsed in
+    case parsed of
+        PVar s ->
+            index s varStack |> Maybe.map (\i -> Var i)
+
+        PLam v body ->
+            if member v varStack then
+                Nothing
+
+            else
+                buildLambda_ body (v :: varStack) |> Maybe.map (Lam v)
+
+        PApps xs ->
+            List.map (\x -> buildLambda_ x varStack) xs |> transform |> Maybe.andThen flattenApplications
+
+
+transform : List (Maybe a) -> Maybe (List a)
+transform list =
+    case list of
+        [] ->
+            Just []
+
+        x :: xs ->
+            Maybe.map2 (\y ys -> y :: ys) x (transform xs)
+
+
+flattenApplications : List Lambda -> Maybe Lambda
+flattenApplications lambdas =
+    case lambdas of
+        [] -> Nothing
+        x :: [] -> Just x
+        x :: y :: ys -> Just (flattenApplications_ (App x y) ys)
+
+flattenApplications_ : Lambda -> List Lambda -> Lambda
+flattenApplications_ lambda lambdas = case lambdas of
+    [] -> lambda
+    x :: xs -> flattenApplications_ (App lambda x) xs
+
+
+index : a -> List a -> Maybe Int
+index x xs =
+    case xs of
+        [] ->
+            Nothing
+
+        y :: ys ->
+            if x == y then
+                Just 0
+
+            else
+                index x ys |> Maybe.map (\i -> i + 1)
